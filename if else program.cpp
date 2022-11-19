c8fc0778ce7720c22c7195fcf3051c234664d734
hardikrathibhl <94820528+Hardik8824@users.noreply.github.com>
2022-11-19 17:34:20 +0530

    Add files via upload

---
+++ if else program.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	// determine no. is positive or negative 
+	
+//	int a;
+	
+//	cout<<"Enter any Number :";
+//	cin>>a;
+//	
+//	if (a>0){
+//		cout<<"a is positive" ; 
+//	}
+
+//	else {
+//		cout <<"a is negative";
+//	}
+	
+	
+	//compare 2 number which is greater 
+	
+//	int a,b;
+//	
+//	cout<<"Enter A :"<<endl;
+//	cin>>a;
+//	
+//	cout<<"Enter B :"<<endl;
+//	cin>>b;
+//	
+//	if (a>b){
+//		cout << "A is greater "<<endl;
+//	}
+//	
+//	if (b>a){
+//		cout <<"B is greater"<<endl;
+//	}
+
+	int a;
+	
+	cout<<"Enter value of A :"<<endl;
+	cin>>a;
+	
+	if (a>0){
+		cout<<"A is positive ";
+	}
+	
+	else if (a<0){
+		cout<<"A is negative";
+	}
+	
+	else {
+		cout<<"A is 0";
+	}
+
+}
